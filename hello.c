4732fde4ceb5aaa40eea90cbe3d665353bb3fa06
Erik2109 <69768997+Erik2109@users.noreply.github.com>
2020-08-20 10:31:36 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Erik Beyer
+ * ebeyer2@huskers.unl.edu
+ * Date: 2020/08/16
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Erik Beyer\n");
+  printf("Computer Engineering\n");
+
+  return 0;
+}
